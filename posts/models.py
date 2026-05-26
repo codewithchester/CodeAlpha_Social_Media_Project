d25af73985da0b99a3ae26678003811b5b876140
Amegbor Emmanuel Kwadwo <emmanuelamegbor26@gmail.com>
2026-05-26 19:45:28 +0000

    created a Comment model and registered it

--- posts/models.py
+++ posts/models.py
@@ -19,3 +19,13 @@ class Post(models.Model):
     
     def __str__(self):
         return f"{self.author.username}: {self.content[:50]}"
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.author.username}: {self.content[:30]}"
+    
